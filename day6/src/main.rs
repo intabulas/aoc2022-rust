173ace5da46475b264536b5694153c43627fb4e1
Mark Lussier <mlussier@gmail.com>
2022-12-06 07:15:20 -0700

    day 6 first pass

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,23 @@
+fn solve(input: &str, distinct: usize) -> Option<usize> {
+    for i in 0..input.len() {
+        let mut buf = 0_u32;
+        let chars = &input[i..i + distinct];
+        for c in chars.chars() {
+            buf |= 1 << (c as u8 - b'a');
+        }
+        if buf.count_ones() == distinct as u32 {
+            return Some(i + distinct);
+        }
+    }
+    None
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+    let result = solve(input, 4);
+
+    println!("Part 1: {}", result.unwrap());
+
+    let result = solve(input, 14);
+    println!("Part 1: {}", result.unwrap());
 }
